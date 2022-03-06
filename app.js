3153d1e20f8ee1a836f2618863224f39fbf350e5
Jason <jimanproductions@gmail.com>
2022-03-06 11:51:40 +0100

    added get innerhtml

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-
-
 //initializing variables
 let notAFanTemp = 'I am not a fan of ';
-let fanSubject = ['dogs', 'cats', 'books', 'cars', 'trains', 'planes', 'words', 'javascript']
+let animals = ['dogs', 'cats', 'elephants', 'moose', 'ducks', 'cows', 'sheep'];
+let vehicles = ['cars', 'trains', 'planes', 'boats', 'helicopters'];
+let words = ['words', 'sentences', 'adjectives', 'pronouns', 'participles'];
+let concepts = ['concepts', 'diversions', 'particularities', 'permiance'];
+
+let display = document.getElementById("display");
 
 //arrow function for selecting random array element.
 let selectRandArray = (arr) => {
@@ -12,4 +15,10 @@ let selectRandArray = (arr) => {
 
 
 //initalizing quote variable with concatenation of notAFanTemp and fanSubject
-let quote = notAFanTemp + fanSubject[selectRandArray(fanSubject)];
+let genQuote = (arr) => {
+    let quote = notAFanTemp + arr[selectRandArray(arr)];
+    display.innerHTML = quote;
+    console.log(quote)
+    return quote;
+}
+
