869ebe663db50d141c790af87fa247e094b133ee
Jason <jimanproductions@gmail.com>
2022-03-06 19:55:59 +0100

    big commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # frontend_assignment1
 
-This is a project for IDG1011 Front-end Web Development. In this project you are able to get a quote from Kanye using the Kanye API.
+This is a project for IDG1011 Front-end Web Development. In this project you are able to find out about things that Kanye West the rapper doesn't like. 
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//initalizing const variables
+const STEMPLAYER = 200;
+
 //initializing variables
 let notAFanTemp = 'I am not a fan of ';
 let animals = ['dogs', 'cats', 'elephants', 'moose', 'ducks', 'cows', 'sheep'];
@@ -5,19 +8,35 @@ let vehicles = ['cars', 'trains', 'planes', 'boats', 'helicopters'];
 let words = ['words', 'sentences', 'adjectives', 'pronouns', 'participles'];
 let concepts = ['concepts', 'diversions', 'particularities', 'permiance'];
 
+//selecting various DOM elements
 let display = document.getElementById("display");
+let us = document.getElementById("us");
+let no = document.getElementById("no");
+let uk = document.getElementById("uk");
+let se = document.getElementById("se");
+let dk = document.getElementById("dk");
 
-//arrow function for selecting random array element.
+//simple math conversions are applied to various currencies
+us.innerHTML = STEMPLAYER + " USD" 
+no.innerHTML = STEMPLAYER * 9 + " NOK" 
+uk.innerHTML = (STEMPLAYER/1.5).toFixed(2) + " GBP" 
+dk.innerHTML = STEMPLAYER * 8 + " DKK" 
+se.innerHTML = STEMPLAYER * 10 + " SEK" 
+
+//function for selecting random array element.
 let selectRandArray = (arr) => {
     let arrSize = arr.length; 
     return Math.floor(Math.random() * arrSize);
 }
 
 
-//initalizing quote variable with concatenation of notAFanTemp and fanSubject
+//function which calls on random array element to generate a new sentence.
 let genQuote = (arr) => {
-    let quote = notAFanTemp + arr[selectRandArray(arr)];
+    //concatenates sentence with ending
+    let quote = '"' + notAFanTemp + arr[selectRandArray(arr)] + '."';
+    //displays using DOM element
     display.innerHTML = quote;
+    //console log for testing purposes.
     console.log(quote)
     return quote;
 }
